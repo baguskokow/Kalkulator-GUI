9602c2ed3a975664214ee759819a180fcfbc07a5
Koko <baguskokolearn@gmail.com>
2026-05-19 22:24:48 +0700

    Fix arithmetic

--- src/com/calculator/model/Arithmetic.java
+++ src/com/calculator/model/Arithmetic.java
@@ -12,83 +12,59 @@ import java.util.Arrays;
 import java.text.DecimalFormat;
 
 class Arithmetic {
-	ArrayList<String> stringNumbers; 
+	ArrayList<Double> operand = new ArrayList<Double>(); 
 	ArrayList<Character> operatorCharacter = new ArrayList<Character>();
 	double result;
 
 	public Arithmetic(String argument) {
-		String[] temp = argument.split("[X\\-\\+\\/]");
-		this.stringNumbers = new ArrayList<String>(Arrays.asList(temp));
-		
-		for(char c : argument.toCharArray()) {
-			if((c == '+' || c == '-' || c == '/' || c == 'X')) {
-				operatorCharacter.add(c);
+		for(int i = 0; i < argument.length(); i++) {
+			if(argument.charAt(i) == '+' || argument.charAt(i) == '-' || argument.charAt(i) == '/' || argument.charAt(i) == 'X') {
+				operatorCharacter.add(argument.charAt(i));
+			} else {
+				operand.add((double) (argument.charAt(i) - '0'));
 			}
 		}
 
-		result = Double.parseDouble(stringNumbers.get(0));
-		division();
-		multiplication();
-		addition();
-		substraction();
+		calculated();
+		result = operand.get(0);
+		counting();
 	}
 
 	public String getResult() {
 		return String.valueOf(result).replaceAll("\\.0", "");
 	}
 
-	public void addition() {
+	public void counting() {
 		for(int i = 0; i < operatorCharacter.size(); i++) {
-			char tempOperator = operatorCharacter.get(i);
-
-			if(tempOperator == '+') {
-				result += Double.parseDouble(stringNumbers.get(i + 1));
+			if(operatorCharacter.get(i) == '+') {
+				result += operand.get(i + 1);
+			} else if(operatorCharacter.get(i) == '-'){
+				result -= operand.get(i + 1);
 			}
 		}
 	}
 	
-	public void substraction() {
+	public void calculated() {
 		for(int i = 0; i < operatorCharacter.size(); i++) {
 			char tempOperator = operatorCharacter.get(i);
+			double tempResult = 0;
 
-			if(tempOperator == '-') {
-				result -= Double.parseDouble(stringNumbers.get(i + 1));
-			}
-		}
-	}
+			if(tempOperator == '/' || tempOperator == 'X') {
+				double leftNumber = operand.get(i);
+				double rightNumber = operand.get(i+1);
 
-	public void division() {
-		int indexOperator;
-		for(int i = 0; i < operatorCharacter.size(); i++) {
-			char tempOperator = operatorCharacter.get(i);
+				if(tempOperator == '/') {
+					tempResult = leftNumber / rightNumber;
+				} else if(tempOperator == 'X') {
+					tempResult = leftNumber * rightNumber;
+				}
 
-			if(tempOperator == '/') {
-				double leftNumber = Double.parseDouble(stringNumbers.get(i));
-				double rightNumber = Double.parseDouble(stringNumbers.get(i+1));
-				result = leftNumber / rightNumber;
-
-				stringNumbers.remove(i + 1);
+				operand.set(i, tempResult);
+				operand.remove(i + 1);
 				operatorCharacter.remove(i);
 				i--;
-			}
-		}
-	}
+			} 
 
-	public void multiplication() {
-		int indexOperator;
-		for(int i = 0; i < operatorCharacter.size(); i++) {
-			char tempOperator = operatorCharacter.get(i);
-
-			if(tempOperator == 'X') {
-				double leftNumber = Double.parseDouble(stringNumbers.get(i));
-				double rightNumber = Double.parseDouble(stringNumbers.get(i+1));
-				double resultKali = leftNumber * rightNumber;
-				stringNumbers.set(i, String.valueOf(resultKali));
-				stringNumbers.remove(i + 1);
-				operatorCharacter.remove(i);
-				this.result = resultKali;
-				i--;
-			}
 		}
 	}
 }
